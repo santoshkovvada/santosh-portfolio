a3e84ca0c764ca68d96584de00cf15a8268514a6
git user <git user@gmail.com>
2025-07-12 17:01:38 +0530

    Add portfolio sections and GitHub Pages config

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import About from './components/About';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Santosh Kumar | DevOps Engineer</h1>
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
     </div>
   );
 }
-
 export default App;
+
+
+
+
+
+/*
+#import logo from './logo.svg';
+#import './App.css';
+
+#function App() {
+  #return (
+    #<div className="App">
+      #<header className="App-header">
+        #<img src={logo} className="App-logo" alt="logo" />
+        #<p>
+          #Edit <code>src/App.js</code> and save to reload.
+        #</p>
+        #<a
+          #className="App-link"
+          #href="https://reactjs.org"
+          #target="_blank"
+          #rel="noopener noreferrer"
+        #>
+         # Learn React
+        #</a>
+      #</header>
+    #</div>
+  #);
+#}
+
+#export default App;
+*/ 
